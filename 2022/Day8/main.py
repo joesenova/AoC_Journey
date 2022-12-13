a3810614518e76d1edb4de05526ce7cc0ca62cef
Johan Kasselman <johankasselman@live.com>
2022-12-13 08:47:32 +0200

    Part1 DONE for day8

--- 2022/Day8/main.py
+++ 2022/Day8/main.py
@@ -6,11 +6,52 @@ import sys
 from api import get_input
 
 
+def calc_vid_tree(line) -> None:
+    ref_tree = line[0][0]
+    line[0][1] = True
+    for tree in line[1:]:
+        if tree[0] > ref_tree:
+            ref_tree = tree[0]
+            tree[1] = True
+
+
+def calc_vis_trees(grid: t.List[t.List[t.List[int, bool]]]) -> int:
+    """
+    :param grid:
+    """
+    total = 0
+    for line in grid:
+        for tree in line:
+            if tree[1]:
+                total += 1
+    return total
+
+
 def part1(lines: t.List[str]) -> int:
     """
     :param lines:
     """
-    pass
+    grid = [[[int(a), False] for a in line.strip()] for line in lines]
+    grid_y = []
+    for x in range(0, len(grid[0])):
+        row = []
+        for y in range(0, len(grid[0])):
+            row.append(grid[y][x])
+        grid_y.append(row)
+
+    for line in grid:
+        calc_vid_tree(line)
+        line.reverse()
+        calc_vid_tree(line)
+
+    for line in grid_y:
+        calc_vid_tree(line)
+        line.reverse()
+        calc_vid_tree(line)
+
+    vis_trees = calc_vis_trees(grid)
+
+    return vis_trees
 
 
 def part2(lines: t.List[str]) -> int:
