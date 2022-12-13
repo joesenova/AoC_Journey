20b9d9729227ae06ffd99a6c6748123d55ae73c3
Johan Kasselman <johankasselman@live.com>
2022-12-13 10:25:22 +0200

    Part2

--- 2022/Day8/main.py
+++ 2022/Day8/main.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 import typing as t
 import sys
-
+import numpy as np
 from api import get_input
 
 
@@ -27,11 +27,12 @@ def calc_vis_trees(grid: t.List[t.List[t.List[int, bool]]]) -> int:
     return total
 
 
-def part1(lines: t.List[str]) -> int:
-    """
-    :param lines:
-    """
-    grid = [[[int(a), False] for a in line.strip()] for line in lines]
+def get_x_y(lines: t.List[str], with_bool: bool = True) -> t.Tuple[t.List, t.List]:
+    """"""
+    if with_bool:
+        grid = [[[int(a), False] for a in line.strip()] for line in lines]
+    else:
+        grid = [[int(a) for a in line.strip()] for line in lines]
     grid_y = []
     for x in range(0, len(grid[0])):
         row = []
@@ -39,6 +40,14 @@ def part1(lines: t.List[str]) -> int:
             row.append(grid[y][x])
         grid_y.append(row)
 
+    return grid, grid_y
+
+
+def part1(lines: t.List[str]) -> int:
+    """
+    :param lines:
+    """
+    grid, grid_y = get_x_y(lines, True)
     for line in grid:
         calc_vid_tree(line)
         line.reverse()
@@ -54,11 +63,31 @@ def part1(lines: t.List[str]) -> int:
     return vis_trees
 
 
+def count_trees(line) -> int:
+    count = 0
+    ref = line[0]
+    for tree in line[1:-1]:
+        if tree > ref:
+            count += 1
+    return count
+
+
 def part2(lines: t.List[str]) -> int:
     """
     :param lines:
     """
-    pass
+    total = 0
+    grid, grid_y = get_x_y(lines, False)
+    for x in range(1, len(grid[0])-1):
+        total += count_trees(grid[x])
+        grid[x].reverse()
+        total += count_trees(grid[x])
+
+        total += count_trees(grid_y[x])
+        grid_y[x].reverse()
+        total += count_trees(grid_y[x])
+
+    return total
 
 
 def main() -> None:
@@ -75,5 +104,5 @@ if __name__ == "__main__":
     sys.exit(main())
 
 # Win Result
-# part1(lines)=
-# part2(lines)=
+# part1(lines)=1560
+# part2(lines)=252000
