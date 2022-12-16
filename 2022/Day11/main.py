9f5412ad8ddeecaf37b8afcbe5c0891720ce0335
Johan Kasselman <johankasselman@live.com>
2022-12-16 03:32:59 +0200

    Complete day 12 p1 and p2 Did Day11 p1

--- 2022/Day11/main.py
+++ 2022/Day11/main.py
@@ -2,17 +2,85 @@
 from __future__ import annotations
 import typing as t
 import sys
+from copy import copy
+
 from api import get_input
+from more_itertools import chunked
 
 
-def part1(lines: t.List[str]) -> int:
+class Monkeys:
 	""""""
-	pass
+
+	def __init__(self):
+		""""""
+		self.all_monkeys: t.List[Monkey] = []
+		self.monkey: t.Union[Monkey, None] = None
+
+	def set_monkey(self, monkey: Monkey):
+		""""""
+		self.monkey = monkey
+
+	def calc_worry_with_operator(self, worry):
+		""""""
+		if self.monkey.operator[0] == 'old':
+			val1 = worry
+		else:
+			val1 = int(self.monkey.operator[0])
+
+		if self.monkey.operator[2] == 'old':
+			val2 = worry
+		else:
+			val2 = int(self.monkey.operator[2])
+
+		if self.monkey.operator[1] == '+':
+			return val1 + val2
+		elif self.monkey.operator[1] == '*':
+			return val1 * val2
+
+	def test_worry(self, worry):
+		""""""
+		if worry % self.monkey.test == 0:
+			return self.monkey.to_true_monkey
+
+		return self.monkey.to_false_monkey
+
+	def inspect(self):
+		""""""
+		items_copy = copy(self.monkey.items)
+		for item in self.monkey.items:
+			items_copy.remove(item)
+			self.monkey.inspections += 1
+			worry = self.calc_worry_with_operator(item) // 3
+			to_monkey = self.test_worry(worry)
+			self.all_monkeys[to_monkey].items.append(worry)
+		self.monkey.items = items_copy
 
 
-def part2(lines: t.List[str]) -> int:
+class Monkey(Monkeys):
 	""""""
-	pass
+	def __init__(self, lines: t.List[str], idx: int):
+		""""""
+		super().__init__()
+		self.name: str = f'Monkey_{idx+1}'
+		self.items: t.List[int] = [int(x) for x in lines[1].split(':')[1].strip().split(',')]
+		self.operator: t.List[str] = lines[2].split('=')[1].strip().split(' ')
+		self.test: int = int(lines[3].split(' ')[-1])
+		self.to_monkey_true: int = int(lines[4].split(' ')[-1])
+		self.to_monkey_false: int = int(lines[5].split(' ')[-1])
+		self.inspections: int = 0
+
+
+def part1(monkeys: Monkeys, rounds: int):
+	for r in range(rounds):
+		for monkey in monkeys.all_monkeys:
+			monkeys.set_monkey(monkey)
+			monkeys.inspect()
+
+	sorted_monkeys = [monkey.inspections for monkey in monkeys.all_monkeys]
+	sorted_monkeys.sort()
+	monkey_business = sorted_monkeys[-1] * sorted_monkeys[-2]
+
+	return monkey_business
 
 
 def main() -> None:
@@ -21,8 +89,14 @@ def main() -> None:
 	with open("input.txt") as my_file:
 		lines = my_file.read().splitlines()
 
-		print(f"{part1(lines)=}")
-		print(f"{part2(lines)=}")
+		# Add all monkeys to Monkeys Class
+		monkeys = Monkeys()
+		for idx, data in enumerate(chunked(lines, 7)):
+			monkey = Monkey(data, idx)
+			monkeys.all_monkeys.append(monkey)
+
+		# Run Part with all monkeys
+		print(f"{part1(monkeys, 20)=}")
 
 
 if __name__ == "__main__":
@@ -30,5 +104,5 @@ if __name__ == "__main__":
 	sys.exit(main())
 
 # Win Result
-# part1(lines)=
-# part2(lines)=
+# part1(monkeys)=113232
+# part2(monkeys)=
--- 2022/Day12/main.py
+++ 2022/Day12/main.py
@@ -3,26 +3,143 @@ from __future__ import annotations
 import typing as t
 import sys
 from api import get_input
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
-def part1(lines: t.List[str]) -> int:
+DICT = {}
+LABELS = {}
+GRID = None
+
+
+def to_str(y, x, GRID) -> str:
 	""""""
-	pass
+	DICT[f'{y},{x}'] = y, x
+	LABELS[f'{y},{x}'] = f"{y, x}: {GRID[y][x]}"
+	return f'{y},{x}'
 
 
-def part2(lines: t.List[str]) -> int:
+def part1(G, source, destination):
 	""""""
-	pass
+	sp = nx.shortest_path(G, f'{source[1]},{source[0]}',  f'{destination[1]},{destination[0]}')
+	ys = [int(co_ord.split(',')[0]) for co_ord in sp]
+	xs = [int(co_ord.split(',')[1]) for co_ord in sp]
+	plt.plot(xs, ys, linestyle='dashed')
+	plt.axis('off')
+	plt.show()
+
+	return len(sp)
+
+
+def part2(G, GRID, destination):
+	""""""
+	lengths = []
+	sources = []
+	co_ords = {}
+	for y, row in enumerate(GRID):
+		for x, _ in enumerate(row):
+			if row[x] == 1:
+				sources.append((x, y))
+
+	for source in sources:
+		try:
+			distance = nx.shortest_path_length(G, f'{source[1]},{source[0]}', f'{destination[1]},{destination[0]}')
+			lengths.append(distance)
+			co_ords[distance] = f'{source[1]},{source[0]}'
+		except nx.NetworkXNoPath as e:
+			continue
+
+	source = co_ords[min(lengths)]
+	sp = nx.shortest_path(G, source,  f'{destination[1]},{destination[0]}')
+	ys = [int(co_ord.split(',')[0]) for co_ord in sp]
+	xs = [int(co_ord.split(',')[1]) for co_ord in sp]
+	plt.plot(xs, ys, linestyle='dashed')
+	plt.axis('off')
+	plt.show()
+
+	return min(lengths)
 
 
 def main() -> None:
 	""" Entrypoint into the solution.
 	"""
+	G = nx.DiGraph()
+
 	with open("input.txt") as my_file:
 		lines = my_file.read().splitlines()
+		GRID = [[ord(l)-96 for l in ln] for ln in lines]
+		the_map = GRID
+		found_start = False
+		found_end = False
 
-		print(f"{part1(lines)=}")
-		print(f"{part2(lines)=}")
+		for idx_y, row in enumerate(the_map):
+			if -13 in the_map[idx_y]:
+				found_start = True
+				source = the_map[idx_y].index(-13), idx_y
+				the_map[source[1]][source[0]] = ord("a")-96
+
+			if -27 in the_map[idx_y]:
+				found_end = True
+				destination = the_map[idx_y].index(-27), idx_y
+				the_map[destination[1]][destination[0]] = ord("z")-96
+
+			if found_start and found_end:
+				break
+	for y in range(0, len(GRID)):
+		for x in range(0, len(GRID[0])):
+			c = GRID[y][x]
+			if 0 < x < len(GRID[0]) - 1:
+				l = GRID[y][x - 1]
+				if c + 1 == l or c >= l:
+					c_node = to_str(y, x, GRID)
+					n_node = to_str(y, x - 1, GRID)
+					G.add_edge(c_node, n_node)
+				r = GRID[y][x + 1]
+				if c + 1 == r or c >= r:
+					c_node = to_str(y, x, GRID)
+					n_node = to_str(y, x + 1, GRID)
+					G.add_edge(c_node, n_node)
+			elif x == 0:
+				r = GRID[y][x + 1]
+				if c + 1 == r or c >= r:
+					c_node = to_str(y, x, GRID)
+					n_node = to_str(y, x + 1, GRID)
+					G.add_edge(c_node, n_node)
+			elif x == len(GRID[0]) - 1:
+				l = GRID[y][x - 1]
+				if c + 1 == l or c >= l:
+					c_node = to_str(y, x, GRID)
+					n_node = to_str(y, x - 1, GRID)
+					G.add_edge(c_node, n_node)
+
+			if 0 < y < len(GRID) - 1:
+				d = GRID[y + 1][x]
+				if c + 1 == d or c >= d:
+					c_node = to_str(y, x, GRID)
+					n_node = to_str(y + 1, x, GRID)
+					G.add_edge(c_node, n_node)
+				u = GRID[y - 1][x]
+				if c + 1 == u or c >= u:
+					c_node = to_str(y, x, GRID)
+					n_node = to_str(y - 1, x, GRID)
+					G.add_edge(c_node, n_node)
+			elif y == 0:
+				d = GRID[y + 1][x]
+				if c + 1 == d or c >= d:
+					c_node = to_str(y, x, GRID)
+					n_node = to_str(y + 1, x, GRID)
+					G.add_edge(c_node, n_node)
+			elif y == len(GRID) - 1:
+				u = GRID[y - 1][x]
+				if c + 1 == u or c >= u:
+					c_node = to_str(y, x, GRID)
+					n_node = to_str(y - 1, x, GRID)
+					G.add_edge(c_node, n_node)
+
+	print(f'{part1(G, source, destination)=}')
+	print(f'{part2(G, GRID, destination)=}')
+
+	plt.close()
 
 
 if __name__ == "__main__":
@@ -30,5 +147,5 @@ if __name__ == "__main__":
 	sys.exit(main())
 
 # Win Result
-# part1(lines)=
-# part2(lines)=
+# part1(lines)=426
+# part2(lines)=418
